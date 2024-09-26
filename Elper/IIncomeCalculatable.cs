2b48f8a0a0a19970d9a84b77bc3ffb96a8079515
nao-toyo <46580960+nao-toyo@users.noreply.github.com>
2024-09-26 16:30:29 +0900

    First commit

---
+++ Elper/IIncomeCalculatable.cs
@@ -0,0 +1,15 @@
+﻿// ============================================================
+// Project Name   : Elper
+// Author         : naoyuki29
+// License        : MIT (See LICENSE included in project)
+// ============================================================
+
+namespace Elper
+{
+
+public interface IIncomeCalculatable
+{
+    double CalculateIncome(int year);
+}
+
+}
